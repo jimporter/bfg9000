d13bda655356a8f57da34b6690b3205e3c3f3716
Jim Porter <jporter@mozilla.com>
2015-06-11 00:38:51 -0500

    Provide a richer find() function with support for multiple base dirs and non-recursive finding

--- bfg9000/rules.py
+++ bfg9000/rules.py
@@ -175,16 +175,28 @@ def global_options(build, env, options, lang):
         build.global_options[lang] = []
     build.global_options[lang].extend(shell_listify(options))
 
+def _walk_flat(path):
+    names = os.listdir(path)
+    dirs, nondirs = [], []
+    for name in names:
+        if os.path.isdir(os.path.join(path, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+    yield path, dirs, nondirs
+
 @builtin
-def find(build_inputs, env, base='.', name='*', type=None):
+def find(build_inputs, env, base='.', name='*', type=None, flat=False):
     results = []
-    for path, dirs, files in os.walk(base):
-        if type != 'f':
-            results.extend((
-                os.path.join(path, i) for i in fnmatch.filter(dirs, name)
-            ))
-        if type != 'd':
-            results.extend((
-                os.path.join(path, i) for i in fnmatch.filter(files, name)
-            ))
+    for b in iterate(base):
+        generator = _walk_flat(b) if flat else os.walk(b)
+        for path, dirs, files in generator:
+            if type != 'f':
+                results.extend((
+                    os.path.join(path, i) for i in fnmatch.filter(dirs, name)
+                ))
+            if type != 'd':
+                results.extend((
+                    os.path.join(path, i) for i in fnmatch.filter(files, name)
+                ))
     return results
