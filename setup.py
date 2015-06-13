ffe556a32e259c6e99c77b403d5863f2a679946f
Jim Porter <jporter@mozilla.com>
2015-06-13 02:29:27 -0500

    Add missing lxml dependency

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from bfg9000.version import __version__
 setup(
     name='bfg9000',
     version=__version__,
-    license='BSD',
 
     author='Jim Porter',
     author_email='porterj@alum.rit.edu',
+    license='BSD',
 
     packages=find_packages(exclude=['test']),
+    install_requires=['lxml'],
     entry_points={
         'console_scripts': ['bfg9000=bfg9000.driver:main'],
     },
