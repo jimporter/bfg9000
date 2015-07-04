ff67d4511bcdf58038bf87f5f6339f8c884195ee
Jim Porter <jporter@mozilla.com>
2015-07-03 23:42:12 -0500

    Move directory validation into a single parse_args() function

--- bfg9000/driver.py
+++ bfg9000/driver.py
@@ -26,66 +26,68 @@ def samefile(path1, path2):
         # be necessary for Windows with Python 2.x.
         return os.path.realpath(path1) == os.path.realpath(path2)
 
-def validate_build_directories(args):
-    if not args.srcdir:
-        raise ValueError('at least one of srcdir or builddir must be defined')
+def parse_args(parser, args=None, namespace=None):
+    def check_dir(path, check_exist=False):
+        if not os.path.exists(path):
+            raise ValueError('{} does not exist'.format(path))
+        if not os.path.isdir(path):
+            parser.error('{} is not a directory'.format(path))
 
-    if args.builddir:
-        if not os.path.exists(args.srcdir):
-            raise ValueError('{} does not exist'.format(args.srcdir))
-        if not os.path.isdir(args.srcdir):
-            raise ValueError('{} is not a directory'.format(args.srcdir))
-    else:
-        if os.path.exists(os.path.join(args.srcdir, bfgfile)):
-            args.builddir = '.'
+    args = parser.parse_args(args, namespace)
+
+    if not args.regenerate:
+        if not args.srcdir:
+            parser.error('at least one of srcdir or builddir must be defined')
+
+        if args.builddir:
+            check_dir(args.srcdir, check_exist=True)
         else:
-            args.srcdir, args.builddir = '.', args.srcdir
+            args.builddir = '.'
+            if is_srcdir(args.srcdir):
+                args.srcdir, args.builddir = args.builddir, args.srcdir
 
-    if os.path.exists(args.builddir):
-        if samefile(args.srcdir, args.builddir):
-            raise ValueError('source and build directories must be different')
-        if not os.path.isdir(args.builddir):
-            raise ValueError('{} is not a directory'.format(args.builddir))
+        if os.path.exists(args.builddir):
+            check_dir(args.builddir)
+            if samefile(args.srcdir, args.builddir):
+                parser.error('source and build directories must be different')
 
-    if not is_srcdir(args.srcdir):
-        raise ValueError('source directory must contain a build.bfg file')
-    if is_srcdir(args.builddir):
-        raise ValueError(
-            'build directory must not contain a build.bfg file'
-        )
+        if not is_srcdir(args.srcdir):
+            parser.error('source directory must contain a build.bfg file')
+        if is_srcdir(args.builddir):
+            parser.error('build directory must not contain a build.bfg file')
 
-    if not os.path.exists(args.builddir):
-        os.mkdir(args.builddir)
-    args.srcdir = os.path.abspath(args.srcdir)
-    args.builddir = os.path.abspath(args.builddir)
+        if not os.path.exists(args.builddir):
+            os.mkdir(args.builddir)
+        args.srcdir = os.path.abspath(args.srcdir)
+        args.builddir = os.path.abspath(args.builddir)
+    else:
+        args.srcdir, args.builddir = None, args.srcdir
+        if args.srcdir:
+            parser.error('source directory cannot be passed when regenerating')
+        if not args.builddir:
+            args.builddir = '.'
 
-def validate_regen_directories(args):
-    # The "source" directory will become our builddir, since it only makes sense
-    # to pass a single dir here.
-    if args.builddir:
-        raise ValueError(
-            'source directory must not be defined when regenerating'
-        )
-    args.srcdir, args.builddir = None, os.path.abspath(args.srcdir or '.')
+        check_dir(args.builddir, check_exist=True)
+        args.builddir = os.path.abspath(args.builddir)
+
+    return args
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('srcdir', nargs='?', help='source directory')
     parser.add_argument('builddir', nargs='?', help='build directory')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s ' + __version__)
     parser.add_argument('--backend', default='make', help='backend')
     parser.add_argument('--prefix', default='/usr', help='installation prefix')
     parser.add_argument('--regenerate', action='store_true',
                         help='regenerate build files')
-    parser.add_argument('--version', action='version',
-                        version='%(prog)s ' + __version__)
 
-    args = parser.parse_args()
+    args = parse_args(parser)
     try:
         if args.regenerate:
-            validate_regen_directories(args)
             env = Environment.load(args.builddir)
         else:
-            validate_build_directories(args)
             env = Environment(
                 bfgpath=os.path.realpath(sys.argv[0]),
                 srcdir=args.srcdir,
@@ -96,13 +98,12 @@ def main():
             )
             env.save(args.builddir)
     except Exception as e:
-        parser.error(e)
+        sys.stderr.write('{}: {}\n'.format(parser.prog, e))
         return 1
 
     build = BuildInputs()
     os.chdir(env.srcdir)
     execfile(os.path.join(env.srcdir, bfgfile), builtins.bind(build, env))
 
-    backend = importlib.import_module('.backends.{}'.format(env.backend),
-                                      'bfg9000')
+    backend = importlib.import_module('.' + env.backend, 'bfg9000.backends')
     backend.write(env, build)
