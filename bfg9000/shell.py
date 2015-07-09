15fad0d1c1a093bae6949bf68a6a0bebccb42ae5
Jim Porter <jporter@mozilla.com>
2015-07-09 01:11:48 -0500

    Provide better implementations of shell.split and shell.quote

--- bfg9000/shell.py
+++ bfg9000/shell.py
@@ -1,7 +1,30 @@
+import platforms
+import re
 import sys
-from shlex import split
+from shlex import shlex
 
-if sys.hexversion >= 0x030300F0:
-    from shlex import quote
+def split(s):
+    if not isinstance(s, basestring):
+        raise TypeError('expected a string')
+    lexer = shlex(s, posix=True)
+    lexer.commenters = ''
+    lexer.whitespace_split = True
+    return list(lexer)
+
+_bad_chars = re.compile(r'[^\w@%+=:,./-]')
+
+def quote_posix(s):
+    if not s:
+        return "''"
+    if _bad_chars.search(s) is None:
+        return s
+    return "'" + s.replace("'", "'\"'\"'") + "'"
+
+def quote_windows(s):
+    # TODO: Implement this!
+    return s
+
+if platforms.platform_name() == 'windows':
+    quote = quote_windows
 else:
-    from pipes import quote
+    quote = quote_posix
