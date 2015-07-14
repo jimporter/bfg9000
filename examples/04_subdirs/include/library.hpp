1e13a0f19d47e2f9dfb8e3952ca2149323c00e00
Jim Porter <jporter@mozilla.com>
2015-07-13 21:20:19 -0500

    Fix building some of the test shared libs on Windows

--- examples/04_subdirs/include/library.hpp
+++ examples/04_subdirs/include/library.hpp
@@ -1,6 +1,16 @@
 #ifndef INC_LIBRARY_HPP
 #define INC_LIBRARY_HPP
 
-void hello();
+#if defined(_WIN32)
+#  ifdef _WINDLL
+#    define LIB_PUBLIC __declspec(dllexport)
+#  else
+#    define LIB_PUBLIC __declspec(dllimport)
+#  endif
+#else
+#  define LIB_PUBLIC
+#endif
+
+void LIB_PUBLIC hello();
 
 #endif
