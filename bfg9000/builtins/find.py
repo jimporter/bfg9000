9fc0e25fcaa005625da54801d78148b97a1b2980
Jim Porter <jporter@mozilla.com>
2015-08-09 16:48:03 -0500

    Always use POSIX paths in find_files()
    This makes the results in build scripts more predictable, allowing users to
    filter them without having to use os.path.join().

--- bfg9000/builtins/find.py
+++ bfg9000/builtins/find.py
@@ -1,5 +1,6 @@
 import fnmatch
 import os
+import posixpath
 
 from . import builtin
 from ..iterutils import iterate
@@ -20,35 +21,54 @@ def write_depfile(path, output, seen_dirs, makeify=False):
                 out.write(os.path.abspath(i), Syntax.target)
                 out.write_literal(':\n')
 
-def _walk_flat(path):
-    names = os.listdir(path)
+def _listdir(path):
     dirs, nondirs = [], []
-    for name in names:
-        if os.path.isdir(os.path.join(path, name)):
-            dirs.append(name)
-        else:
-            nondirs.append(name)
-    yield path, dirs, nondirs
+    try:
+        names = os.listdir(path)
+        for name in names:
+            if os.path.isdir(os.path.join(path, name)):
+                dirs.append(name)
+            else:
+                nondirs.append(name)
+    except:
+        pass
+    return dirs, nondirs
 
+def _walk_flat(top):
+    yield (top,) + _listdir(top)
+
+def _walk_recursive(top):
+    dirs, nondirs = _listdir(top)
+    yield top, dirs, nondirs
+    for name in dirs:
+        path = posixpath.join(top, name)
+        if not os.path.islink(path):
+            for i in _walk_recursive(path):
+                yield i
+
+# FIXME: Always use forward slashes for path delimiters here? That way filtering
+# the results after the fact is predictable.
 def _find_files(paths, name, type, flat):
     results = []
     seen_dirs = []
+
+    # "Does the walker choose the path, or the path the walker?" - Garth Nix
+    walker = _walk_flat if flat else _walk_recursive
+
+    def _filter_join(base, files, name):
+        return (posixpath.join(base, i) for i in fnmatch.filter(files, name))
+
     for p in iterate(paths):
         if type != 'f' and fnmatch.fnmatch(p, name):
             results.append(p)
 
-        generator = _walk_flat(p) if flat else os.walk(p)
+        generator = walker(p)
         for path, dirs, files in generator:
             seen_dirs.append(path)
-
             if type != 'f':
-                results.extend(
-                    os.path.join(path, i) for i in fnmatch.filter(dirs, name)
-                )
+                results.extend(_filter_join(path, dirs, name))
             if type != 'd':
-                results.extend(
-                    os.path.join(path, i) for i in fnmatch.filter(files, name)
-                )
+                results.extend(_filter_join(path, files, name))
 
     return results, seen_dirs
 
