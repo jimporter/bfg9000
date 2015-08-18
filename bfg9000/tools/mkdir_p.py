d269880fb3e3be7924b2c00c0eafd22df3f9cd21
Jim Porter <jporter@mozilla.com>
2015-08-17 23:11:46 -0500

    Add missing file

---
+++ bfg9000/tools/mkdir_p.py
@@ -0,0 +1,12 @@
+class MkdirP(object):
+    def __init__(self, env):
+        default = 'gmkdir' if env.platform.name == 'windows' else 'mkdir'
+        self.command_name = env.getvar('MKDIR_P', default + ' -p')
+        self.command_var = 'mkdir_p'
+
+    def command(self, cmd, path):
+        return [cmd, path]
+
+    # TODO: Remove this once we rewrite the install command.
+    def copy_command(self, cmd, src, dst):
+        return cmd + ' ' + dst + ' && cp -r ' + src + '/* ' + dst
