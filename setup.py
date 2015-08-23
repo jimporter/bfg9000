191d05e1ce3049f27e2fc85f1d031e6a4e398e53
Jim Porter <jporter@mozilla.com>
2015-08-23 18:46:13 -0500

    Update setup.py to serve/push docs

--- setup.py
+++ setup.py
@@ -1,5 +1,34 @@
-from setuptools import setup, find_packages
+import subprocess
+from setuptools import setup, find_packages, Command
 from bfg9000.version import version
+
+class doc_serve(Command):
+    description = 'serve the documentation locally'
+    user_options = [
+        ('dev-addr=', None, 'address to host the documentation on'),
+    ]
+
+    def initialize_options(self):
+        self.dev_addr = '0.0.0.0:8000'
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        subprocess.call(['mkdocs', 'serve', '--dev-addr=' + self.dev_addr])
+
+class doc_deploy(Command):
+    description = 'push the documentation to GitHub'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        subprocess.call(['mkdocs', 'gh-deploy', '--clean'])
 
 setup(
     name='bfg9000',
@@ -27,4 +56,9 @@ setup(
     },
 
     test_suite='test',
+
+    cmdclass={
+        'doc_serve': doc_serve,
+        'doc_deploy': doc_deploy,
+    },
 )
