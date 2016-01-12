504d1c8127b6153dd2b828e8c481376c83f4662c
Jim Porter <jporter@mozilla.com>
2016-01-12 13:10:41 -0800

    Add another CI build variant for Windows

--- scripts/appveyor_install_libogg.bat
+++ scripts/appveyor_install_libogg.bat
@@ -1,8 +1,9 @@
 curl -LO http://downloads.xiph.org/releases/ogg/libogg-1.3.2.zip
-7z x -y libogg-1.3.2.zip
+7z x -y libogg-1.3.2.zip > nul
 
 set SRCDIR=libogg-1.3.2
 set PROJDIR=%SRCDIR%\win32\VS2010
+
 msbuild %PROJDIR%\libogg_static.sln
 mkdir %SRCDIR%\lib
 copy %PROJDIR%\Win32\Debug\libogg_static.lib %SRCDIR%\lib\ogg.lib
