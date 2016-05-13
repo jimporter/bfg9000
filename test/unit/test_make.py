85e43a8b99fb84aca71c17ba0983271c596aff49
Jim Porter <jporter@mozilla.com>
2016-05-12 22:44:16 -0500

    Add more tests for the Make/Ninja writers

--- test/unit/test_make.py
+++ test/unit/test_make.py
@@ -3,14 +3,131 @@ import unittest
 from six.moves import cStringIO as StringIO
 
 from bfg9000 import path
+from bfg9000 import safe_str
 from bfg9000.backends.make.syntax import *
+from bfg9000.platforms import platform_name
+
+esc_colon = ':' if platform_name() == 'windows' else '\\:'
 
 
-class TestMakeSyntax(unittest.TestCase):
-    def test_path_quoting(self):
+def quoted(s):
+    return "'" + s + "'"
+
+
+class TestMakeWriter(unittest.TestCase):
+    # strings
+    def test_write_string_target(self):
         out = Writer(StringIO())
-        p = path.Path('foo', path.Root.srcdir)
-        out.write(p, Syntax.shell)
-
+        out.write('foo: $bar|baz,quux', Syntax.target)
         self.assertEqual(out.stream.getvalue(),
-                         "'" + os.path.join('$(srcdir)', 'foo') + "'")
+                         'foo' + esc_colon + '\\ $$bar|baz,quux')
+
+    def test_write_string_dependency(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar|baz,quux', Syntax.dependency)
+        self.assertEqual(out.stream.getvalue(),
+                         'foo' + esc_colon + '\\ $$bar\\|baz,quux')
+
+    def test_write_string_function(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar|baz,quux', Syntax.function)
+        self.assertEqual(out.stream.getvalue(), quoted('foo: $$bar|baz$,quux'))
+
+    def test_write_string_shell(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar|baz,quux', Syntax.shell)
+        self.assertEqual(out.stream.getvalue(), quoted('foo: $$bar|baz,quux'))
+
+    def test_write_string_clean(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar|baz,quux', Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), 'foo: $$bar|baz,quux')
+
+    # escaped strings
+    def test_write_escaped_string_target(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar|baz,quux'), Syntax.target)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar|baz,quux')
+
+    def test_write_escaped_string_dependency(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar|baz,quux'),
+                  Syntax.dependency)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar|baz,quux')
+
+    def test_write_escaped_string_function(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar|baz,quux'), Syntax.function)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar|baz,quux')
+
+    def test_write_escaped_string_shell(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar|baz,quux'), Syntax.shell)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar|baz,quux')
+
+    def test_write_escaped_string_clean(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar|baz,quux'), Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar|baz,quux')
+
+    # jbos
+    def test_write_jbos_target(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.target)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    def test_write_jbos_dependency(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.dependency)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    def test_write_jbos_function(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.function)
+        self.assertEqual(out.stream.getvalue(), quoted('$$foo') + '$bar')
+
+    def test_write_jbos_shell(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.shell)
+        self.assertEqual(out.stream.getvalue(), quoted('$$foo') + '$bar')
+
+    def test_write_jbos_clean(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    # paths
+    def test_write_path_target(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.target)
+        self.assertEqual(out.stream.getvalue(),
+                         os.path.join('$(srcdir)', 'foo'))
+
+    def test_write_path_dependency(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.dependency)
+        self.assertEqual(out.stream.getvalue(),
+                         os.path.join('$(srcdir)', 'foo'))
+
+    def test_write_path_function(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.function)
+        self.assertEqual(out.stream.getvalue(),
+                         quoted(os.path.join('$(srcdir)', 'foo')))
+
+    def test_write_path_shell(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.shell)
+        self.assertEqual(out.stream.getvalue(),
+                         quoted(os.path.join('$(srcdir)', 'foo')))
+
+    def test_write_path_clean(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.clean)
+        self.assertEqual(out.stream.getvalue(),
+                         os.path.join('$(srcdir)', 'foo'))
--- test/unit/test_ninja.py
+++ test/unit/test_ninja.py
@@ -3,16 +3,107 @@ import unittest
 from six.moves import cStringIO as StringIO
 
 from bfg9000 import path
+from bfg9000 import safe_str
 from bfg9000.backends.ninja.syntax import *
 from bfg9000.platforms import platform_name
 
+quote_char = '"' if platform_name() == 'windows' else "'"
 
-class TestNinjaSyntax(unittest.TestCase):
-    def test_path_quoting(self):
+
+def quoted(s):
+    return quote_char + s + quote_char
+
+
+class TestNinjaWriter(unittest.TestCase):
+    # strings
+    def test_write_string_output(self):
         out = Writer(StringIO())
-        p = path.Path('foo', path.Root.srcdir)
-        out.write(p, Syntax.shell)
+        out.write('foo: $bar', Syntax.output)
+        self.assertEqual(out.stream.getvalue(), 'foo$:$ $$bar')
 
-        quote = '"' if platform_name() == 'windows' else "'"
+    def test_write_string_input(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar', Syntax.input)
+        self.assertEqual(out.stream.getvalue(), 'foo:$ $$bar')
+
+    def test_write_string_shell(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar', Syntax.shell)
+        self.assertEqual(out.stream.getvalue(), quoted('foo: $$bar'))
+
+    def test_write_string_clean(self):
+        out = Writer(StringIO())
+        out.write('foo: $bar', Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), 'foo: $$bar')
+
+    # escaped strings
+    def test_write_escaped_string_output(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar'), Syntax.output)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar')
+
+    def test_write_escaped_string_input(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar'), Syntax.input)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar')
+
+    def test_write_escaped_string_shell(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar'), Syntax.shell)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar')
+
+    def test_write_escaped_string_clean(self):
+        out = Writer(StringIO())
+        out.write(safe_str.escaped_str('foo: $bar'), Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), 'foo: $bar')
+
+    # jbos
+    def test_write_jbos_output(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.output)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    def test_write_jbos_input(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.input)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    def test_write_jbos_shell(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.shell)
+        if platform_name() == 'windows':
+            expected = '$$foo$bar'
+        else:
+            expected = quoted('$$foo') + '$bar'
+        self.assertEqual(out.stream.getvalue(), expected)
+
+    def test_write_jbos_clean(self):
+        out = Writer(StringIO())
+        s = safe_str.jbos('$foo', safe_str.escaped_str('$bar'))
+        out.write(s, Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), '$$foo$bar')
+
+    # paths
+    def test_write_path_output(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.output)
+        self.assertEqual(out.stream.getvalue(), os.path.join('$srcdir', 'foo'))
+
+    def test_write_path_input(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.input)
+        self.assertEqual(out.stream.getvalue(), os.path.join('$srcdir', 'foo'))
+
+    def test_write_path_shell(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.shell)
         self.assertEqual(out.stream.getvalue(),
-                         quote + os.path.join('$srcdir', 'foo') + quote)
+                         quoted(os.path.join('$srcdir', 'foo')))
+
+    def test_write_path_clean(self):
+        out = Writer(StringIO())
+        out.write(path.Path('foo', path.Root.srcdir), Syntax.clean)
+        self.assertEqual(out.stream.getvalue(), os.path.join('$srcdir', 'foo'))
