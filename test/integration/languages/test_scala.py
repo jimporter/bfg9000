1df9d1ec302ac1d30f6a5ddf03f98c81ca20e390
Jim Porter <jporter@mozilla.com>
2016-10-07 03:22:01 -0500

    Disable scala tests on Windows/Mac

--- test/integration/languages/test_scala.py
+++ test/integration/languages/test_scala.py
@@ -3,15 +3,15 @@ import os
 
 from .. import *
 
+if os.getenv('NO_SCALA_TEST') not in ['1', 'true']:
+    class TestScala(IntegrationTest):
+        def __init__(self, *args, **kwargs):
+            IntegrationTest.__init__(
+                self, os.path.join('languages', 'scala'), *args, **kwargs
+            )
 
-class TestScala(IntegrationTest):
-    def __init__(self, *args, **kwargs):
-        IntegrationTest.__init__(
-            self, os.path.join('languages', 'scala'), *args, **kwargs
-        )
-
-    def test_build(self):
-        self.build('program.jar')
-        for i in glob.glob("*.class*"):
-            os.remove(i)
-        self.assertOutput(['scala', 'program.jar'], 'hello from scala!\n')
+        def test_build(self):
+            self.build('program.jar')
+            for i in glob.glob("*.class*"):
+                os.remove(i)
+            self.assertOutput(['scala', 'program.jar'], 'hello from scala!\n')
