9e38f0c54faa4b4bdfa15dd5139d562a0661a2a0
Jim Porter <jporter@mozilla.com>
2017-11-05 01:15:29 -0600

    Add unit tests for versioning builtins

---
+++ test/unit/builtins/test_version.py
@@ -0,0 +1,25 @@
+import unittest
+
+from bfg9000.builtins.version import bfg9000_required_version, bfg9000_version
+from bfg9000.versioning import bfg_version, VersionError
+
+
+class TestRequiredVersion(unittest.TestCase):
+    def test_bfg_version(self):
+        bfg9000_required_version('>=0.1.0')
+        self.assertRaises(VersionError, bfg9000_required_version, '<=0.1.0')
+
+    def test_python_version(self):
+        bfg9000_required_version(python_version='>=2.7.0')
+        self.assertRaises(VersionError, bfg9000_required_version, None,
+                          '<=2.0.0')
+
+    def test_both_versions(self):
+        bfg9000_required_version('>=0.1.0', '>=2.7.0')
+        self.assertRaises(VersionError, bfg9000_required_version, '<=0.1.0',
+                          '<=2.0.0')
+
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        self.assertEqual(bfg9000_version(), bfg_version)
