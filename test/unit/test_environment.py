4419f9986e844f8e81ad7c05dca7ab1cdc5461f7
Jim Porter <jporter@mozilla.com>
2018-02-19 13:05:07 -0600

    Add unit test for upgrading .bfg_environ

---
+++ test/unit/test_environment.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+import sys
+from six import iteritems
+
+from bfg9000.environment import Environment, LibraryMode
+from bfg9000.path import Path, Root, InstallRoot
+from bfg9000.platforms import platform_name
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+test_data_dir = os.path.join(this_dir, '..', 'data')
+
+
+class TestEnvironment(unittest.TestCase):
+    def assertDictsEqual(self, a, b):
+        self.assertEqual(len(a), len(b))
+        for i in a:
+            self.assertEqual(a[i], b[i])
+
+    def test_upgrade_from_v4(self):
+        env = Environment.load(
+            os.path.join(test_data_dir, 'environment', 'v4'),
+            save_on_upgrade=False
+        )
+
+        self.assertEqual(env.bfgdir, Path('/path/to', Root.absolute))
+        self.assertEqual(env.backend, 'make')
+
+        self.assertEqual(env.srcdir, Path('/root/srcdir', Root.absolute))
+        self.assertEqual(env.builddir, Path('/root/builddir', Root.absolute))
+        self.assertDictsEqual(env.install_dirs, {
+            InstallRoot.prefix: Path('/root/prefix', Root.absolute),
+            InstallRoot.exec_prefix: Path('', InstallRoot.prefix),
+            InstallRoot.bindir: Path('bin', InstallRoot.exec_prefix),
+            InstallRoot.libdir: Path('lib', InstallRoot.exec_prefix),
+            InstallRoot.includedir: Path('include', InstallRoot.prefix),
+        })
+
+        self.assertEqual(env.library_mode, LibraryMode(True, False))
+        self.assertEqual(env.extra_args, [])
+
+        variables = {u'HOME': u'/home/user'}
+        if platform_name() == 'windows' and sys.version_info[0] == 2:
+            variables = {str(k): str(v) for k, v in iteritems(variables)}
+        self.assertEqual(env.variables, variables)
+
+        self.assertEqual(env.platform.name, 'linux')
