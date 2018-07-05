a76df07c6010999e6d9c3b6a20e6e962bd17410b
Jim Porter <jporter@mozilla.com>
2018-07-04 18:03:54 -0700

    Add more unit tests for the logger

--- test/unit/test_log.py
+++ test/unit/test_log.py
@@ -2,6 +2,7 @@ import logging
 import mock
 import re
 import sys
+import traceback
 import unittest
 from six import assertRegex
 
@@ -24,18 +25,18 @@ class TestStackfulStreamHandler(unittest.TestCase):
             handler.emit(record)
 
         self.assertEqual(record.full_stack, [
-            (this_file, 18, 'test_runtime_error',
+            (this_file, 19, 'test_runtime_error',
              "raise RuntimeError('runtime error')"),
         ])
         self.assertEqual(record.stack_pre, '')
         self.assertEqual(record.stack, (
             '\n' +
-            '  File "{}", line 18, in test_runtime_error\n' +
+            '  File "{}", line 19, in test_runtime_error\n' +
             "    raise RuntimeError('runtime error')"
         ).format(this_file))
         self.assertEqual(record.stack_post, '')
         self.assertEqual(record.user_pathname, this_file)
-        self.assertEqual(record.user_lineno, 18)
+        self.assertEqual(record.user_lineno, 19)
 
     def test_internal_error(self):
         handler = log.StackfulStreamHandler()
@@ -50,13 +51,13 @@ class TestStackfulStreamHandler(unittest.TestCase):
 
         iterutils_file = iterutils.__file__.rstrip('c')
         self.assertEqual(record.full_stack, [
-            (this_file, 43, 'test_internal_error', "iterutils.first(None)"),
+            (this_file, 44, 'test_internal_error', "iterutils.first(None)"),
             (iterutils_file, 48, 'first', 'raise LookupError()'),
         ])
         self.assertEqual(record.stack_pre, '')
         self.assertEqual(record.stack, (
             '\n' +
-            '  File "{}", line 43, in test_internal_error\n' +
+            '  File "{}", line 44, in test_internal_error\n' +
             "    iterutils.first(None)"
         ).format(this_file))
         self.assertEqual(record.stack_post, (
@@ -65,7 +66,7 @@ class TestStackfulStreamHandler(unittest.TestCase):
             "    raise LookupError()"
         ).format(iterutils_file))
         self.assertEqual(record.user_pathname, this_file)
-        self.assertEqual(record.user_lineno, 43)
+        self.assertEqual(record.user_lineno, 44)
 
     def test_syntax_error(self):
         handler = log.StackfulStreamHandler()
@@ -90,3 +91,61 @@ class TestStackfulStreamHandler(unittest.TestCase):
         self.assertEqual(record.stack_post, '')
         self.assertEqual(record.user_pathname, 'file.py')
         self.assertEqual(record.user_lineno, 1)
+
+    def test_no_user_stack(self):
+        handler = log.StackfulStreamHandler()
+        try:
+            iterutils.first(None)
+        except LookupError:
+            exc = sys.exc_info()
+            record = logging.LogRecord(
+                'name', 'level', 'pathname', 1, 'msg', [],
+                exc[0:2] + (exc[2].tb_next,)
+            )
+        with mock.patch.object(logging.StreamHandler, 'emit'), \
+             mock.patch('logging.root.handle'):  # noqa
+            handler.emit(record)
+
+        iterutils_file = iterutils.__file__.rstrip('c')
+        self.assertEqual(record.full_stack, [
+            (iterutils_file, 48, 'first', 'raise LookupError()'),
+        ])
+        self.assertEqual(record.stack, (
+            '\n' +
+            '  File "{}", line 48, in first\n' +
+            "    raise LookupError()"
+        ).format(iterutils_file))
+
+        self.assertFalse(hasattr(record, 'stack_pre'))
+        self.assertFalse(hasattr(record, 'stack_post'))
+        self.assertFalse(hasattr(record, 'user_pathname'))
+        self.assertFalse(hasattr(record, 'user_lineno'))
+
+
+class TestInit(unittest.TestCase):
+    def test_colors(self):
+        with mock.patch('logging.addLevelName'), \
+             mock.patch('logging.root.addHandler'):  # noqa
+            with mock.patch('colorama.init') as colorama:
+                log.init()
+                colorama.assert_called_once_with()
+
+            with mock.patch('colorama.init') as colorama:
+                log.init(color='always')
+                colorama.assert_called_once_with(strip=False)
+
+            with mock.patch('colorama.init') as colorama:
+                log.init(color='never')
+                colorama.assert_called_once_with(strip=True, convert=False)
+
+    def test_warn_once(self):
+        with mock.patch('logging.addLevelName'), \
+             mock.patch('logging.root.addHandler'), \
+             mock.patch('colorama.init'):  # noqa
+            with mock.patch('warnings.filterwarnings') as filterwarnings:
+                log.init()
+                filterwarnings.assert_not_called()
+
+            with mock.patch('warnings.filterwarnings') as filterwarnings:
+                log.init(warn_once=True)
+                filterwarnings.assert_called_once_with('once')
