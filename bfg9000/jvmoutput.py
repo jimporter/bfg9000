fb00258e9fcbc2d0554b72da1c0367ee093eee9a
Jim Porter <jporter@mozilla.com>
2018-10-11 17:38:00 -0700

    Fix jvmoutput script to detect class files written with newer JDKs

--- bfg9000/jvmoutput.py
+++ bfg9000/jvmoutput.py
@@ -1,3 +1,5 @@
+import errno
+import os
 import re
 import subprocess
 import sys
@@ -5,7 +7,15 @@ import sys
 from .arguments import parser as argparse
 from .app_version import version
 
-_class_re = re.compile(r"\[wrote (?:RegularFileObject\[|'[^']*' to )([^\]]*)")
+# This captures a few kinds of messages emitted via `javac -verbose` (or
+# scalac or whatever):
+#   JDK 1-8:  [wrote RegularFileObject[path]]
+#   JDK 9-10: [wrote DirectoryFileObject[bad:path]]
+#   JDK 11+:  [wrote path]
+#   Scala:    [wrote 'thing' to path]
+_class_re = re.compile(
+    r"\[wrote ((?:Regular|Directory)FileObject\[|'[^']*' to |)([^\]]*)"
+)
 
 
 def main():
@@ -16,21 +26,36 @@ def main():
     )
     parser.add_argument('--version', action='version',
                         version='%(prog)s ' + version)
-    parser.add_argument('-o', type=argparse.FileType('w'), required=True,
+    parser.add_argument('-o', type=argparse.FileType('w'), default=sys.stdout,
                         dest='output', help=('the output file to list the ' +
                                              'generated .class files'))
     parser.add_argument('command', nargs=argparse.REMAINDER, metavar='COMMAND',
                         help='the command to execute')
     args = parser.parse_args()
 
-    p = subprocess.Popen(args.command, universal_newlines=True,
-                         stderr=subprocess.PIPE)
+    if len(args.command) == 0:
+        parser.error('command required')
+
+    try:
+        p = subprocess.Popen(args.command, universal_newlines=True,
+                             stderr=subprocess.PIPE)
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            parser.exit(66, 'command not found: {}\n'.format(args.command[0]))
+        raise  # pragma: no cover
+
     for line in p.stderr:
         if line[0] != '[':
-            sys.stdout.write(line)
+            sys.stderr.write(line)
             continue
 
         m = _class_re.match(line)
         if m:
-            args.output.write(m.group(1) + '\n')
+            kind = m.group(1)
+            class_file = m.group(2)
+            # JDK 9-10 emits broken paths; fix them. For more info, see
+            # <https://bugs.openjdk.java.net/browse/JDK-8194893>.
+            if kind == 'DirectoryFileObject[':
+                class_file = class_file.replace(':', os.path.sep)
+            args.output.write(class_file + '\n')
     return p.wait()
---
+++ test/integration/test_jvmoutput.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from six import assertRegex
+
+from . import *
+
+
+def print_stderr_args(message):
+    return [sys.executable, '-c', 'import sys; sys.stderr.write("{}")'.format(
+        message.replace('"', '\\"').replace('\n', '\\n')
+    )]
+
+
+class TestJvmOutput(TestCase):
+    def test_no_args(self):
+        self.assertPopen(['bfg9000-jvmoutput'], returncode=2)
+
+    def test_nonexistent_command(self):
+        self.assertPopen(['bfg9000-jvmoutput', 'nonexist'], returncode=66)
+
+    def test_jdk_8(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput'] + print_stderr_args(
+                '[wrote RegularFileObject[foo/bar.class]]\n'
+            ),
+            'foo/bar.class\n'
+        )
+
+    def test_jdk_10(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput'] + print_stderr_args(
+                '[wrote DirectoryFileObject[bad:foo/bar.class]]\n'
+            ),
+            os.path.join('bad', 'foo/bar.class') + '\n'
+        )
+
+    def test_jdk_11(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput'] + print_stderr_args(
+                '[wrote foo/bar.class]\n'
+            ),
+            'foo/bar.class\n'
+        )
+
+    def test_scala(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput'] + print_stderr_args(
+                "[wrote 'bar' to foo/bar.class]\n"
+            ),
+            'foo/bar.class\n'
+        )
+
+    def test_multiple(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput'] + print_stderr_args(
+                '[wrote foo/bar.class]\n' +
+                '[wrote baz/quux.class]\n'
+            ),
+            'foo/bar.class\nbaz/quux.class\n'
+        )
+
+    def test_non_verbose(self):
+        self.assertOutput(
+            ['bfg9000-jvmoutput', '-o', os.devnull] + print_stderr_args(
+                '[wrote foo/bar.class]\n' +
+                'bleep\n'
+            ),
+            'bleep\n'
+        )
