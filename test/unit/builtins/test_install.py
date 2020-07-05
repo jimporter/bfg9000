7b25e4d1ebbd465741ed0b566e1d5f07db7c8058
Jim Porter <itsjimporter@gmail.com>
2020-07-04 22:08:44 -0700

    Add unit tests for install command with make/ninja backends

--- test/unit/builtins/test_install.py
+++ test/unit/builtins/test_install.py
@@ -1,8 +1,10 @@
 from unittest import mock
 
-from .common import BuiltinTest
+from .common import AlwaysEqual, BuiltinTest
 
-from bfg9000.builtins import default, install  # noqa
+from bfg9000.backends.make import syntax as make
+from bfg9000.backends.ninja import syntax as ninja
+from bfg9000.builtins import compile, default, install, link, packages, project  # noqa
 from bfg9000.file_types import Executable, Phony
 from bfg9000.path import Path, Root, InstallRoot
 
@@ -64,3 +66,58 @@ class TestInstall(BuiltinTest):
             installed = Executable(Path('exe', InstallRoot.bindir), None)
             self.assertEqual(self.context['install'](exe), installed)
             self.assertEqual(m.call_count, 1)
+
+
+class TestMakeBackend(BuiltinTest):
+    def test_no_install(self):
+        makefile = make.Makefile(None)
+
+        with mock.patch.object(make.Makefile, 'rule') as mrule, \
+             mock.patch('logging.log'):  # noqa
+            install.make_install_rule(self.build, makefile, self.env)
+            mrule.assert_not_called()
+
+    def test_install(self):
+        makefile = make.Makefile(None)
+        src = self.context['source_file']('main.cpp')
+        exe = self.context['executable']('exe', src)
+        self.context['install'](exe)
+
+        with mock.patch.object(make.Makefile, 'rule') as mrule, \
+             mock.patch('logging.log'):  # noqa
+            install.make_install_rule(self.build, makefile, self.env)
+            self.assertEqual(mrule.mock_calls, [
+                mock.call(target='install', deps='all', phony=True,
+                          recipe=AlwaysEqual()),
+                mock.call(target='uninstall', phony=True, recipe=AlwaysEqual())
+            ])
+
+
+class TestNinjaBackend(BuiltinTest):
+    def test_no_install(self):
+        ninjafile = ninja.NinjaFile(None)
+
+        with mock.patch.object(ninja.NinjaFile, 'build') as mbuild, \
+             mock.patch('logging.log'):  # noqa
+            install.ninja_install_rule(self.build, ninjafile, self.env)
+            mbuild.assert_not_called()
+
+    def test_install(self):
+        ninjafile = ninja.NinjaFile(None)
+        src = self.context['source_file']('main.cpp')
+        exe = self.context['executable']('exe', src)
+        self.context['install'](exe)
+
+        with mock.patch.object(ninja.NinjaFile, 'build') as mbuild, \
+             mock.patch.object(ninja.NinjaFile, 'has_build',
+                               return_value=True), \
+             mock.patch('logging.log'):  # noqa
+            install.ninja_install_rule(self.build, ninjafile, self.env)
+            self.assertEqual(mbuild.mock_calls, [
+                mock.call(output='install', inputs=['all'], implicit=['PHONY'],
+                          order_only=None, rule='command',
+                          variables=AlwaysEqual()),
+                mock.call(output='uninstall', inputs=None, implicit=['PHONY'],
+                          order_only=None, rule='command',
+                          variables=AlwaysEqual()),
+            ])
