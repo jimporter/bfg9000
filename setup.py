74bc863b61d6f3c5294bde7818a304ec9e16cbdf
Jim Porter <itsjimporter@gmail.com>
2020-08-10 00:18:05 -0700

    Add CI for Python 3.8

--- setup.py
+++ setup.py
@@ -159,6 +159,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     packages=find_packages(exclude=['test', 'test.*']),
