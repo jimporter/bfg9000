fc67e977c39e2ab0ea608f0acdf03ba74b25d394
Jim Porter <itsjimporter@gmail.com>
2021-11-21 18:00:21 -0800

    Fix doc_deploy

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ try:
                 info = json.loads(subprocess.run(
                     ['mike', 'list', '-j', alias], universal_newlines=True,
                     check=True, stdout=subprocess.PIPE
-                ))
+                ).stdout)
             except subprocess.CalledProcessError:
                 info = None
 
